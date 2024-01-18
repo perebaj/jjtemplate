7d1d00f6dbe8854e284bc87f037a48908cae0d67
jojo <perebaj@gmail.com>
2024-01-18 01:11:04 -0300

    :sparkles: embed

--- template.go
+++ template.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"embed"
 	"flag"
 	"fmt"
 	"io/fs"
@@ -15,6 +16,9 @@ type Project struct {
 	Name     string
 	Registry string
 }
+
+//go:embed templates/*
+var templatesDir embed.FS
 
 func main() {
 
@@ -32,21 +36,14 @@ func main() {
 	project.Name = *projectName
 	project.Registry = *registryName
 
-	home, err := os.UserHomeDir()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	templatesDir := os.DirFS(home + "/jjtemplate/templates/core")
-	err = walkProject(templatesDir, ".", project, output)
+	err := walkProject(templatesDir, "templates/core", project, output)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	if *compose {
-		templatesDir = os.DirFS(home + "/jjtemplate/templates/compose")
-		err = walkProject(templatesDir, ".", project, output)
+		err = walkProject(templatesDir, "templates/compose", project, output)
 		if err != nil {
 			log.Fatal(err)
 		}
